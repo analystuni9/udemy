6b18566fb24c06b122bb203ac8b39784f04b0683
Danilo Brito <danilo-8@outlook.com>
2023-08-21 04:24:58 -0300

    "atualizações projeto_4"

--- CURSO_JAVASCRIPT_GUANABARA/arquivo.js
+++ CURSO_JAVASCRIPT_GUANABARA/arquivo.js
@@ -1,3 +1,9 @@
-window.alert('Minha primeira mensagem');
-window.confirm('Está gostando de JS?');
-window.prompt('Qual é o seu nome? ')
+// var nome = window.prompt('Qual é o seu nome? '); //vai perguntar seu nome
+// window.alert('É um grande prazer em te conhecer,' + ' ' + nome + '!')
+
+var n1 = Number.parseInt(window.prompt('Digite um número: '));
+var n2 = Number.parseInt(window.prompt('Digite outro número: '));
+var s = n1 + n2;
+window.alert('A soma dos valores é' + ' ' + s);
+var j = 'Javascript'
+window.alert(`Estou aprendendo ${j}`);
