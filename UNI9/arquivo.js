36fd6f5e03c5c7ebbe3363b495a97dac7e911d39
Danilo Brito <danilo-8@outlook.com>
2023-08-20 23:12:14 -0300

    fiz alteração nos arquivos na pasta uni9

---
+++ UNI9/arquivo.js
@@ -0,0 +1,13 @@
+function exibir() {
+    alert('Já me livrei da maldição');
+    alert('Estou aprendendo JS');
+    confirm('Você quer entra no site?');
+    prompt('Digite seu nome:')
+};
+function mudaTexto() {
+      /* abre a página em outra guia do navegador */
+window.open("https://www.significados.com.br/acessibilidade/")
+   };
+   function mudaTexto(){
+      document.getElementById("explica").innerHTML ="Acessibilidade Digital é a eliminação de barreiras na Web. O conceito pressupõe que os sites e portais sejam projetados de modo que todas as pessoas possam perceber, entender, navegar e interagir de maneira efetiva com as páginas."
+   };
