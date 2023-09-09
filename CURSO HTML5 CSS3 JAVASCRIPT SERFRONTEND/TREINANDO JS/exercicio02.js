adc677839201a317e7f8a873f86e3646e1c03ade
Danilo Brito <danilo-8@outlook.com>
2023-09-08 22:43:00 -0300

    fiz alterações nos arquivos da pasta treinando js

--- CURSO HTML5 CSS3 JAVASCRIPT SERFRONTEND/TREINANDO JS/exercicio02.js
+++ CURSO HTML5 CSS3 JAVASCRIPT SERFRONTEND/TREINANDO JS/exercicio02.js
@@ -11,7 +11,7 @@ console.log(teste);
 // Javascript é uma liguagem fracamente tipada isso significa que posso reatribuir um novo valor de uma variavel de outro tipo observe o exemplo acima teste recebe um texto "minha string" logo apos um número "10"
 
 // var pode ser declarada varias vezes e pode receber varios valores
-// let só pode ser declarado uma unica vez porem pode receber outro valor
+// let só pode ser declarada uma unica vez porem pode receber outro valor
 // const vc tem que declarar e já atribuir um valor a ela não pode ser declarada novamente nem atribuir outro valor
 
 // let eu posso declarar e quando eu precisar declaro um valor a ela
