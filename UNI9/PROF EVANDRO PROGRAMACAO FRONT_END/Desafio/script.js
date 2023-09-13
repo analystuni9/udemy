dda6a0902904748a35c476e70819bb0f976560b3
Danilo Brito <danilo-8@outlook.com>
2023-09-13 17:36:20 -0300

    fiz alterações no arquivo desafio prof evandro

--- UNI9/PROF EVANDRO PROGRAMACAO FRONT_END/Desafio/script.js
+++ UNI9/PROF EVANDRO PROGRAMACAO FRONT_END/Desafio/script.js
@@ -32,18 +32,18 @@
 //     }
 //     document.getElementById('view').innerHTML = imagem;
 //  }
-function mudaTexto(id) {
-    let texto
-    if (id == 1) {
-        texto = 'Naruto passa a treinar com o poderoso Jiraya, que vai ajudá-lo a controlar melhor seu chakra e sua relação com a Nove Caudas, ainda que isso exija medidas extremas. Kabuto e um jounin da Vila Oculta da Areia planejam uma invasão da Vila da Folha pelas da Areia e do Som. Os finalistas do exame chunin seguem treinando.'
-        document.getElementById('view2').innerHTML = texto
-    } else if (id == 2) {
-        texto = 'As semelhanças entre o passado de Gaara e Naruto são reveladas. Contudo, o caminho que Gaara seguiu é bem mais sombrio, com o jovem ninja acreditando que seu único propósito é matar aqueles que atrapalhem seus objetivos. As batalhas do exame chunin seguem, com o aguardado confronto entre Sasuke e Gaara. A batalha, porém, é interrompida por Orochimaru e Kabuto, que dão início à invasão da Vila Oculta da Folha.'
-        document.getElementById('view3').innerHTML = texto
-        texto = 'Orochimaru captura o Terceiro Hokage, e uma luta incomparável tem início. Sasuke procura por Gaara, enquanto Sakura, Naruto e Shikamaru tentam alcançá-lo e pará-lo. Lutas se espalham por toda a aldeia, que parece estar sendo destruída. Konohamaru evacua a escola. Agora é Naruto e Gaara que estão frente à frente, e ambos usam os poderes das bestas de caudas que carregam.'
-        document.getElementById('view4').innerHTML = texto
-    }
-}
-function visualizarImagem (x) {
-    document.getElementById('view2').src = x;
+// function mudaTexto(id) {
+//     let texto
+//     if (id == 1) {
+//         texto = 'Naruto passa a treinar com o poderoso Jiraya, que vai ajudá-lo a controlar melhor seu chakra e sua relação com a Nove Caudas, ainda que isso exija medidas extremas. Kabuto e um jounin da Vila Oculta da Areia planejam uma invasão da Vila da Folha pelas da Areia e do Som. Os finalistas do exame chunin seguem treinando.'
+//         document.getElementById('view2').innerHTML = texto
+//     } else if (id == 2) {
+//         texto = 'As semelhanças entre o passado de Gaara e Naruto são reveladas. Contudo, o caminho que Gaara seguiu é bem mais sombrio, com o jovem ninja acreditando que seu único propósito é matar aqueles que atrapalhem seus objetivos. As batalhas do exame chunin seguem, com o aguardado confronto entre Sasuke e Gaara. A batalha, porém, é interrompida por Orochimaru e Kabuto, que dão início à invasão da Vila Oculta da Folha.'
+//         document.getElementById('view3').innerHTML = texto
+//         texto = 'Orochimaru captura o Terceiro Hokage, e uma luta incomparável tem início. Sasuke procura por Gaara, enquanto Sakura, Naruto e Shikamaru tentam alcançá-lo e pará-lo. Lutas se espalham por toda a aldeia, que parece estar sendo destruída. Konohamaru evacua a escola. Agora é Naruto e Gaara que estão frente à frente, e ambos usam os poderes das bestas de caudas que carregam.'
+//         document.getElementById('view4').innerHTML = texto
+//     }
+// }
+function VisualizarImagem (x) {
+    document.getElementById('view-2').src = x;
 }
