3ad6aa0f8a5a9db48c839d1ad4a175c4c6e535d1
danilo <danilo-8@outlook.com>
2023-09-26 03:24:05 -0300

    codigo menu responsivo melhorado sem bugs

--- RESPONSIVIDADE/x.js
+++ RESPONSIVIDADE/x.js
@@ -1,17 +1,17 @@
 function alternar () {
-    let a = document.getElementById('icon-mobile')
-    let b = document.getElementById('menu-mobile')
-    if (b.style.marginRight == '-300px') {
-        b.style.marginRight = '0';
-        a.querySelector('.icon-mobile div:nth-child(1)').style.transformOrigin = '100% 47%';
-        a.querySelector('.icon-mobile div:nth-child(1)').style.transform = 'rotate(-45deg) scaleX(1.25)';
-        a.querySelector('.icon-mobile div:nth-child(2)').style.opacity = '0';
-        a.querySelector('.icon-mobile div:nth-child(3)').style.transformOrigin = ' 100% 0%';
-        a.querySelector('.icon-mobile div:nth-child(3)').style.transform = 'rotate(45deg) scaleX(1.25)';
+    let m = document.getElementById('menu-mobile')
+    let i = document.getElementById('icon-mobile')
+    if (m.style.display == 'none') {
+        m.style.display = 'block'
+        i.querySelector('.icon-mobile div:nth-child(1)').style.transformOrigin = '100% 0%';
+        i.querySelector('.icon-mobile div:nth-child(1)').style.transform = 'rotate(-45deg) scaleX(1.25)';
+        i.querySelector('.icon-mobile div:nth-child(2)').style.display = 'none';
+        i.querySelector('.icon-mobile div:nth-child(3)').style.transformOrigin = '60% 100%';
+        i.querySelector('.icon-mobile div:nth-child(3)').style.transform = 'rotate(45deg) scaleX(1.25)';
     } else {
-        b.style.marginRight = '-300px';
-        a.querySelector('.icon-mobile div:nth-child(1)').style.transform = 'rotate(0) scaleX(1)';
-        a.querySelector('.icon-mobile div:nth-child(2)').style.opacity = '1';
-        a.querySelector('.icon-mobile div:nth-child(3)').style.transform = 'rotate(0) scaleX(1)';
+        m.style.display = 'none';
+        i.querySelector('.icon-mobile div:nth-child(1)').style.transform = 'rotate(0) scaleX(1)'
+        i.querySelector('.icon-mobile div:nth-child(2)').style.display =  'block'
+        i.querySelector('.icon-mobile div:nth-child(3)').style.transform = 'rotate(0) scaleX(1)'
     }
 }
