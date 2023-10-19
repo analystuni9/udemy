f6a88aa88f592cdddb2f79506e03c2c548f966fb
Danilo Brito <danilo-8@outlook.com>
2023-10-18 23:17:35 -0300

    adicionei novos arquivos pasta projeto com o grupo e uni9

---
+++ PROJETO COM O GRUPO UNI9/exercicio06.js
@@ -0,0 +1,9 @@
+function adicao (a, b) {
+    return a + b
+}
+let resultado1 = adicao(2, 2)
+console.log(resultado1)
+
+let multiplicacao = (a, b) => a * b
+let resultado2 = multiplicacao(5, 3)
+console.log(resultado2)
---
+++ PROJETO COM O GRUPO UNI9/revisão10.js
@@ -0,0 +1,28 @@
+// var posso declarar e atribuir valores quantas vezes eu quiser
+// var variavel = 10
+// var variavel = 20
+// variavel = 30
+// variavel = 40
+// console.log(variavel)
+
+
+// let so posso declarar uma unica vez porem posso atribuir varios valores
+// let variavel = 10
+// variavel = 20
+// variavel = 30
+// console.log(variavel)
+
+
+// imutavel ela não muda
+// const variavel = 10
+// console.log(variavel)
+
+
+// array guarda varios valores
+// let variavel = [1, 2, 3, 4, 5]
+// console.log(variavel[3])
+
+
+let variavel = [1, 2, 3, 4, 5]
+let exibir = (numero) => console.log(numero)
+variavel.forEach(exibir)
