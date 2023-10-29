90acd058f9573eb3158b57cabbd8a6e205b46f0f
Danilo Brito <danilo-8@outlook.com>
2023-10-29 16:42:09 -0300

    adicinei a pasta 15/10/23

---
+++ 15.10.23/revisão.js
@@ -0,0 +1,86 @@
+// posso declarar varias vezes
+// posso atribuir um valor varias vezes
+// var variavel = 10
+// var variavel = 20
+// variavel = 30
+// variavel = 40
+// console.log(variavel)
+
+
+// so posso declarar uma unica vez 
+// porem eu posso atribuir um valor varias vezes
+// let variavel = 10
+// variavel = 20
+// variavel = 30
+// console.log(variavel)
+
+// sou imutavel eu nao mudou
+// const variavel = 10
+// console.log(variavel)
+
+// let numeros = [1, 2, 3, 4, 5]
+// console.log(numeros[4])
+
+// let numeros = [1, 2, 3, 4, 5, 6, 7]
+// console.log(numeros.length)
+
+// let minhavar2 = 'minha "string" com aspas simples';
+// let minhavar3 = `minha template literal`;
+
+// let idade = 40;
+// let mensagem1 = "eu tenho" + " " + idade + " " + "anos"
+// let mensagem2 = `eu tenho ${idade} anos`
+// let numero1 = 1
+// let numero2 = 1.1
+// let variavel
+
+// console.log(idade)
+// console.log(mensagem1)
+// console.log(mensagem2)
+// console.log(typeof mensagem1)
+// console.log(typeof mensagem2)
+// console.log(typeof numero1)
+// console.log(typeof numero2)
+// console.log(variavel)
+// let numero3 = "10"
+// let numero4 = "1"
+// console.log(parseInt(numero3))
+// console.log(parseFloat(numero4))
+// let n = 15
+// console.log(n.toString(16))
+// let n = 20
+// n = n +15
+// console.log(n)
+// n += 15
+// console.log(n)
+// n **= 2
+// console.log(n)
+
+// let diasemana = 3
+// if (diasemana == 0) {
+//     console.log("hoje é domingo")
+// } else if (diasemana == 1) {
+//     console.log("hoje é segunda")
+// } else if (diasemana == 2) {
+//     ("hoje é terça")
+// } else if (diasemana == 3) {
+//     console.log("hoje é quarta")
+// } else if (diasemana == 4) {
+//     console.log("hoje é quinta")
+// }
+
+// let indice = 0
+// while (indice < 10) {
+//     indice ++
+//     console.log(indice)
+// }
+// for (i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+// function somar (a, b) {
+//     return a + b
+// }
+// console.log(somar(3, 5))
+// const multiplicacao = (a, b) => a * b
+// console.log(multiplicacao(3, 5))
+
