a51318bbb6abe7a6b57eaace4ddeb9406524aa8b
Danilo Brito <danilo-8@outlook.com>
2023-11-03 18:36:29 -0300

    adicinei arquivo index carrossel alternado

--- 15.10.23/x2.js
+++
@@ -1,8 +0,0 @@
-// let frutas = ['maçã', 'banana', 'pera', 'abacaxi', 'melancia']
-// frutas.forEach((radio) => {
-    //     console.log(radio)
-    // })
-let frutas = ['maçã', 'banana', 'pera', 'abacaxi', 'melancia']
-for (i = 0; i <= frutas.length; i++) {
-    console.log(frutas[i])
-}
---
+++ PROJETO EM LIVE/revisão17.js
@@ -0,0 +1,6 @@
+let numeros = [1, 2, 3, 4, 5]
+let i = 0
+for (i = 0; i < numeros.length; ++i) {
+    console.log(numeros[i])
+}
+console.log(numeros[2], "-> Estou fora do for")
