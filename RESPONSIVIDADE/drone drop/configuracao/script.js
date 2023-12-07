7a05c9c87858a0ed8975b22e980ffc3c40da21a0
Danilo Brito <danilo-8@outlook.com>
2023-12-07 06:36:33 -0300

    fiz varias alterações

--- RESPONSIVIDADE/drone drop/configuracao/script.js
+++ RESPONSIVIDADE/drone drop/configuracao/script.js
@@ -4,9 +4,21 @@ function aplicarConfiguracoes() {
     let corDaFonte = document.getElementById('fontColor').value;
     let tipoDeFonte = document.getElementById('fontFamily').value;
     let main = document.querySelector('main');
+    let language = document.getElementById('language').value;
 
     // Aplicar as configurações ao main e ao h1
     main.style.backgroundColor = corDeFundo;
     document.querySelector('h1').style.color = corDaFonte;
-    main.style.fontFamily = tipoDeFonte;
+    document.body.style.fontFamily = tipoDeFonte;
+
+    // Adicionar lógica para alterar a linguagem
+    if (language === 'en') {
+        // Defina as configurações específicas para o idioma inglês
+        window.alert('Configurações aplicadas para inglês');
+    } else if (language === 'pt') {
+        // Defina as configurações específicas para o idioma português
+        window.alert('Configurações aplicadas para português');
+    } else {
+        // Lógica para outros idiomas, se necessário
+    }
 }
--- RESPONSIVIDADE/drone drop/login/script.js
+++ RESPONSIVIDADE/drone drop/login/script.js
@@ -1,13 +1,30 @@
-let iconmenu = document.querySelector('.icon-mobile')
-let menumobile = document.querySelector('.menu-mobile')
+let iconmenu = document.querySelector('.icon-mobile');
+let menumobile = document.querySelector('.menu-mobile');
+let btn = document.querySelector('.btn');
+
 let animation = () => {
     if (iconmenu.classList.contains('animation_x')) {
-        iconmenu.classList.remove('animation_x')
-        menumobile.classList.remove('exibir')
-
+        iconmenu.classList.remove('animation_x');
+        menumobile.classList.remove('exibir');
     } else {
-        iconmenu.classList.add('animation_x')
-        menumobile.classList.add('exibir')
+        iconmenu.classList.add('animation_x');
+        menumobile.classList.add('exibir');
     }
 }
-iconmenu.addEventListener('click', animation)
+
+iconmenu.addEventListener('click', animation);
+
+function verificarCredenciais() {
+ 
+    var usuario = document.getElementById('email').value;
+    var senha = document.getElementById('password').value;
+
+    if (usuario === 'admin@exemple.com' && senha === 'uninove123') {
+        alert('Acesso Liberado');
+    } else {
+        alert('Credenciais Inválidas. Tente novamente.');
+    }
+}
+
+btn.addEventListener('click', verificarCredenciais);
+
