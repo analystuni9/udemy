e53c39d5a83bb8942b5420726ccdfb79a64e7ceb
Danilo Brito <danilo-8@outlook.com>
2024-03-04 06:10:45 -0300

    Adicionei a pasta curso_python

---
+++ curso_python/aula02.py
@@ -0,0 +1,8 @@
+# Função print ultilizada para exibir as coisas na tela
+# Argumentos são passados para funções
+# Quebra de linha
+# \r\n -> CRLF
+# \n -> LF
+print(12, 38, sep="-")
+print(56, 78, sep=',')
+print(9, 10, sep='/', end='\n')
---
+++ curso_python/aula07.py
@@ -0,0 +1,17 @@
+# Variáveis são usadas para salvar algo na memória do computador
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável)
+nome_completo = "Danilo Brito"
+print(nome_completo)
+soma = 2 + 2
+print(soma)
+int_um = type('1')
+print(int_um)
+
+nome = "Danilo"
+idade = 30
+maiorDeIdade = idade >= 18
+print("Nome: ", nome, "idade: ", idade)
+print("É maior? ", maiorDeIdade)
