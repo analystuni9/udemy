c3309af2fb5af6ea176584fc6ccc0aa5f559a3f4
Danilo Brito <danilo-8@outlook.com>
2024-03-04 07:08:11 -0300

    Fiz correções na pasta curso_python

---
+++ curso_python/aula03.py
@@ -0,0 +1,9 @@
+# Python = Linguagem de programação
+# Tipo de tipagem = Dinâmica / Forte
+# str -> string -> texto 
+print(123)
+# Aspas simples
+# Aspas duplas
+# Escape
+print("Danilo \"Brito\" ")
+print('"Danilo" Brito')
---
+++ curso_python/aula04.py
@@ -0,0 +1,22 @@
+# Tipos int e float
+# int -> Número inteiro
+# O tipo int representa qualquer número
+# Positivo ou negativo int sem sinal é considerado positivo
+
+# Float -> Número com ponto flutuante
+# O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante
+# float sem sinal é considerado positivo
+print(1.1, 2.1) #float
+
+print(1) #int
+print(-1) #int
+print(0) #int
+
+# A função type mostra o tipo que o Python 
+# inferiu ao valor
+print(type("Danilo"))
+print(type(1))
+print(type(0))
+print(type(-1))
+print(type(1.1))
