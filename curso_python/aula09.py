972c1ec7bef2bd03168afd859a110e1bd166d3bc
Danilo Brito <danilo-8@outlook.com>
2024-03-04 21:22:42 -0300

    Atualizações de  arquivos no Curso Python

---
+++ curso_python/aula09.py
@@ -0,0 +1,21 @@
+# Operadores aritimeticos
+adicao = 10 + 10
+print("Adição ", adicao)
+
+subtracao = 10 - 5
+print("Subtracao ", subtracao)
+
+multiplicacao = 10 * 10
+print("Multiplicacao ", multiplicacao)
+
+divisao = 10 / 10 # -> sempre o retorno e float não importa se vc só colocou numero inteiro
+print("Divisão ", divisao)
+
+divisaoInteira = 10 // 2.2 # -> sempre corta as casas decimais
+print("Divisão inteira ", divisaoInteira)
+
+exponenciacao = 2 ** 10
+print("Exponenciação ", exponenciacao)
+
+modulo = 55 % 2 # -> resto da divisão
+print("Modulo ", modulo)
